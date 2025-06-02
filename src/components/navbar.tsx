8b2929569569e4c616259b006e6c6ac7caf6764e
KhyamAllami <kallami@gmail.com>
2025-06-02 20:03:21 +0300

    ajnas and maqamat updates

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
 
   const currentPath = usePathname().split("/")[1];
 
-  const admin = false;
+  const admin = true;
 
   const selectedSayr: Sayr | null = (selectedMaqam && maqamSayrId) ? selectedMaqam.getSuyūr().find((sayr) => sayr.id === maqamSayrId) || null : null
 
