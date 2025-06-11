84219f4641c18b4b2bb06858d545881df8e1fb35
Ibrahim El Khansa <itzbahb@gmail.com>
2025-06-12 02:04:01 +0300

    removed whitespaces from tuning system id + reordered id

--- src/models/TuningSystem.ts
+++ src/models/TuningSystem.ts
@@ -38,7 +38,7 @@ export default class TuningSystem {
     referenceFrequencies: { [noteName: string]: number },
     defaultReferenceFrequency: number
   ) {
-    this.id = `${titleEnglish}-${year}-${creatorEnglish}`;
+    this.id = `${creatorEnglish}-(${year})-${titleEnglish}`.replaceAll(" ", "");
     this.titleEnglish = titleEnglish;
     this.titleArabic = titleArabic;
     this.year = year;
