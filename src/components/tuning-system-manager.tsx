789f126570d684b725058bb0d743e4d63c014513
KhyamAllami <kallami@gmail.com>
2025-08-11 14:58:08 +0300

    More spelling changes

--- src/components/tuning-system-manager.tsx
+++ src/components/tuning-system-manager.tsx
@@ -98,7 +98,7 @@ export default function TuningSystemManager({ admin }: { admin: boolean }) {
     "ʿajam ʿushayrān",
     "kawasht",
     "rāst",
-    "zirguleh",
+    "zīrgūleh",
     "dūgāh",
     "kurdī",
     "būselīk/ʿushshāq",
